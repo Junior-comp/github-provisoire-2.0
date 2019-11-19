f3e041a1afa6d9a00f25e9059cd0c6ff5fc1c878
goffinet nicolas <191492@umons.ac.be>
2019-11-19 16:57:58 +0100

    unknow changes

--- Master_Of_Mons/desktop/src/be/ac/umons/mom/g05/desktop/DesktopLauncher.java
+++ Master_Of_Mons/desktop/src/be/ac/umons/mom/g05/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Master of Mons";
-		config.width=496;
-		config.height=272;
+		config.width=750;
+		config.height=750;
 		new LwjglApplication(new MasterOfMons(), config);
 	}
 }
